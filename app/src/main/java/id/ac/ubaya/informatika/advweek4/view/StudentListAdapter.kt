e2b1adfcdb93533408dd2bb8764d4d5e529d6277
imaderama <s160419075@student.ubaya.ac.id>
2021-09-02 16:41:26 +0800

    Add StudentListAdapter.kt

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListAdapter.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/StudentListAdapter.kt
@@ -3,12 +3,21 @@ package id.ac.ubaya.informatika.advweek4.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import id.ac.ubaya.informatika.advweek4.R
 import id.ac.ubaya.informatika.advweek4.model.Student
+import kotlinx.android.synthetic.main.fragment_student_detail.view.*
+import kotlinx.android.synthetic.main.student_list_item.view.*
 
 class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
     class StudentViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    fun updateStudentList(newStudentList:List<Student>){
+        studentList.clear()
+        studentList.addAll(newStudentList)
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -18,7 +27,15 @@ class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        with(holder.view){
+            txtId.text = studentList[position].id
+            txtName.text = studentList[position].name
 
+            btnDetail.setOnClickListener {
+                val action = StudentListFragmentDirections.actionStudentDetail()
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
