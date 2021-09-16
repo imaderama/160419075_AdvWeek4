ca03854bc1552c0875e8e1d3990ae829609289be
imaderama <s160419075@student.ubaya.ac.id>
2021-09-16 11:53:14 +0800

    edit Util.kt

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/util/Util.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/util/Util.kt
@@ -1,9 +1,12 @@
 package id.ac.ubaya.informatika.advweek4.util
 
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import id.ac.ubaya.informatika.advweek4.R
+import java.lang.Exception
 
 fun ImageView.loadImage(url:String, progressBar:ProgressBar){
     Picasso.get()
@@ -11,5 +14,14 @@ fun ImageView.loadImage(url:String, progressBar:ProgressBar){
             .resize(400,400)
             .centerCrop()
             .error(R.drawable.ic_baseline_error_24)
-            .into(this)
+            .into(this, object: Callback {
+                override fun onSuccess() {
+                    progressBar.visibility = View.GONE
+                }
+
+                override fun onError(e: Exception?) {
+
+                }
+
+            })
 }
