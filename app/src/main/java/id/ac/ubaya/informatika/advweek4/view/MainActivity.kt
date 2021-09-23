caf284f411f1619a9b37719c17e22840b7d19cab
imaderama <s160419075@student.ubaya.ac.id>
2021-09-23 14:09:28 +0800

    Week 7, Edit MainActivity.kt

--- app/src/main/java/id/ac/ubaya/informatika/advweek4/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4/view/MainActivity.kt
@@ -2,11 +2,41 @@ package id.ac.ubaya.informatika.advweek4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import id.ac.ubaya.informatika.advweek4.R
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var observable = Observable.just("hellow", "world", "!!")
+        val observer = object: Observer<String> {
+            override fun onSubscribe(d: Disposable?) {
+                Log.d("Messages", "Start subscribe")
+            }
+
+            override fun onNext(t: String?) {
+                Log.d("Messages", "data captured: ${t.toString()}")
+            }
+
+            override fun onError(e: Throwable?) {
+                Log.e("Messages", "error: ${e!!.message.toString()}")
+            }
+
+            override fun onComplete() {
+                Log.d("Messages", "Completed")
+            }
+        }
+
+        observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer)
     }
 }
